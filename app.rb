917ad7da957094a5aa6e7232a6034bff7053b816
Josh Gwinn <jdgwinn911@gmail.com>
2018-09-10 11:24:00 -0400

    upload for the boys XD

--- app.rb
+++ app.rb
@@ -2,7 +2,10 @@ require 'sinatra'
 require 'mysql2'
 require 'aws-sdk'
 require_relative 'phone_book.rb'
-require_relative 'local_ENV.rb'
+# require_relative 'local_ENV.rb'
+load 'local_ENV.rb' if File.exist?('local_ENV.rb')
+enable :sessions
+
 client = Mysql2::Client.new(:host => ENV['endpoint'], :username => ENV['username'], :password => ENV['password'], :database => ENV['db'])
 #results = client.query("SELECT * FROM `phoneybook`")
 get '/' do
@@ -24,10 +27,13 @@ post '/phoney1' do
   password = params[:password]
   passwordconfig = params[:passwordconfig]
   arr = []
-  client.query("INSERT INTO `user`(id, username, password) VALUES(UUID(), '#{username}', '#{password}')")
   x = client.query("SELECT `id` FROM `user` WHERE username = '#{username}' AND password = '#{password}'")
   x.each do |c|
     arr << c['id']
+  end
+  unless arr.length > 0
+    session[:error] = "invalid username or password"
+    redirect '/'
   end
   session[:user_id] = arr.join('')
   # client.query("INSERT INTO contacts() VALUES()" )
@@ -41,10 +47,10 @@ get '/phoney2' do
 end
 
 post '/phoney2' do
-  username = params[:username]
-  password = params[:password]
-  
-  client.query("INSERT INTO `user`(id, username, password) VALUES(UUID(), '#{username}', '#{password}')")
+  mkusername = params[:mkusername]
+  mkpassword = params[:mkpassword]
+
+  client.query("INSERT INTO `user`(id, username, password) VALUES(UUID(), '#{mkusername}', '#{mkpassword}')")
   redirect '/'
 end
 
@@ -64,5 +70,28 @@ post '/phonedash' do
   Zip = params[:Zip]
   id = session[:user_id]
   client.query("INSERT INTO `contacts`(First_Name, Last_Name, Phone_Number, Street_Address, City, State, Zip, owner) VALUES('#{First_Name}', '#{Last_Name}', '#{Phone_Number}', '#{Street_Address}', '#{City}', '#{ State}', '#{Zip}', '#{id}')")
-  redirect '/'
+  redirect '/contacts'
+end
+
+get '/contacts' do
+  contacts = []
+
+ v = client.query("SELECT * FROM `contacts` WHERE owner = '#{session[:user_id]}'")
+ v.each do |z|
+  aray =[]
+  aray << z['First_Name']
+  aray << z['Last_Name']
+  aray << z['Phone_Number']
+  aray << z['Street_Address']
+  aray << z['City']
+  aray << z['State']
+  aray << z['Zip']
+  contacts << aray
+ end
+  erb :contacts_page, locals:{contacts: contacts || []}
+
+end
+
+post '/contacts' do
+  redirect '/phonedash'
 end
