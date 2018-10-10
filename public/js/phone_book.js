2e33c829cd3e3a1c2d3bacaa00b37afcdc8b5216
Josh Gwinn <jdgwinn911@gmail.com>
2018-10-10 11:11:00 -0400

    fixed some bugs

--- public/js/phone_book.js
+++ public/js/phone_book.js
@@ -18,8 +18,26 @@ function searchfunk() {
 
 
 function disableButton(){
-    var dash = document.getElementById("dash");
- document.getElementById("disable").disabled = "disabled";
-    dash.submit();
+  var dash = document.getElementById("dash");
+  var x = document.getElementById("fname");
+  var y = document.getElementById("lname");
+  var z = document.getElementById("pnum");
+  var g = document.getElementById("strtadd");
+  var h = document.getElementById("city");
+  var l = document.getElementById("state");
+  var k = document.getElementById("zip"); 
+  var counter = 0;
+  var arr = [x, y, z, g, h, l, k];
+
+  for (var d = 0; d < arr.length; d++){
+    if (arr[d].value.length > 1) {
+      counter++;
+    };
+    if (counter == 6) {
+      document.getElementById("disable").disabled = "disabled";
+      dash.submit();
+
+    };
+  }
 }
 
